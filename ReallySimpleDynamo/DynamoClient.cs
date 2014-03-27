f8c4254ba78a2a2bf078dca15fe03ac8bdd78ce2
David Whitney <david@davidwhitney.co.uk>
2014-03-27 11:27:26 +0000

    guard checks for client instantiation.

---
+++ ReallySimpleDynamo/DynamoClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ReallySimpleDynamo
+{
+    public class DynamoClient
+    {
+        public ClientConfiguration ClientConfiguration { get; set; }
+
+        public DynamoClient(ClientConfiguration clientConfiguration)
+        {
+            if (clientConfiguration == null) throw new ArgumentNullException("clientConfiguration");
+
+            ClientConfiguration = clientConfiguration;
+        }
+
+        public void Get<T>(string tableName, string key)
+        {
+            if (string.IsNullOrWhiteSpace(tableName)) throw new ArgumentNullException("tableName");
+            if (string.IsNullOrWhiteSpace(key)) throw new ArgumentNullException("key");
+
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ClientConfiguration
+    {
+    }
+}
