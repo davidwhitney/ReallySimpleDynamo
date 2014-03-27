e0fb4be83b664019339c00ec9fb83b3531422543
David Whitney <david@davidwhitney.co.uk>
2014-03-27 14:10:46 +0000

    Borrowed date formats from official sdk

--- ReallySimpleDynamo/RequestTemplater.cs
+++ ReallySimpleDynamo/RequestTemplater.cs
@@ -8,6 +8,8 @@ namespace ReallySimpleDynamo
         // TODO: Implement hashing algos lifted from official SDK.
         public HttpWebRequest CreateRequestTemplate(ClientConfiguration configuration)
         {
+
+
             var contentSh256 = "";
             var dateBase = DateTime.Now;
             var timestamp = "";
@@ -33,5 +35,40 @@ namespace ReallySimpleDynamo
 
             return req;
         }
+
+        /// <summary>
+        /// The string representing Url Encoded Content in HTTP requests
+        /// </summary>
+        public const string UrlEncodedContent = "application/x-www-form-urlencoded; charset=utf-8";
+
+        /// <summary>
+        /// The GMT Date Format string. Used when parsing date objects
+        /// </summary>
+        public const string GMTDateFormat = "ddd, dd MMM yyyy HH:mm:ss \\G\\M\\T";
+
+        /// <summary>
+        /// The ISO8601Date Format string. Used when parsing date objects
+        /// </summary>
+        public const string ISO8601DateFormat = "yyyy-MM-dd\\THH:mm:ss.fff\\Z";
+
+        /// <summary>
+        /// The ISO8601Date Format string. Used when parsing date objects
+        /// </summary>
+        public const string ISO8601DateFormatNoMS = "yyyy-MM-dd\\THH:mm:ss\\Z";
+
+        /// <summary>
+        /// The ISO8601 Basic date/time format string. Used when parsing date objects
+        /// </summary>
+        public const string ISO8601BasicDateTimeFormat = "yyyyMMddTHHmmssZ";
+
+        /// <summary>
+        /// The ISO8601 basic date format. Used during AWS4 signature computation.
+        /// </summary>
+        public const string ISO8601BasicDateFormat = "yyyyMMdd";
+
+        /// <summary>
+        /// The RFC822Date Format string. Used when parsing date objects
+        /// </summary>
+        public const string RFC822DateFormat = "ddd, dd MMM yyyy HH:mm:ss \\G\\M\\T";
     }
 }
